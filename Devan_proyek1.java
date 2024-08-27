2caf55785928f57b6b095a30fd918526e156324f
devansanto <devan.putra.hersusanto@students.untidar.ac.id>
2024-08-27 14:40:56 +0700

    first commit

---
+++ Devan_proyek1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devan_proyek1;
+
+/**
+ *
+ * @author asus1
+ */
+public class Devan_proyek1 {
+
+    public static void main(String[] args) {
+         // Mendefinisikan variabel dengan infornasi yabg diinginkan
+        String Nama = "Devan Putra Hersusanto";
+        String NPM = "2340506072";
+        String Alamat = "Kota Magelang";
+        String Nomor_HP = "085891924066";
+        
+        // Menampilkan informasi ke layar
+        System.out.println("- Nama : " + Nama);
+        System.out.println("- NPM : " + NPM);
+        System.out.println("- Alamat : " + Alamat);
+        System.out.println("- Nomor_HP : " + Nomor_HP);
+    }
+}
